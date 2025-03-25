c1d183a6f31ddac73440606821c5e1c03565ea60
Rafael Cardoso <racasantos@icloud.com>
2025-03-25 09:26:09 -0400

    olha o tamanho do meu saco

--- src/main/java/us/com/rclabs/app/routes/RouteConfig.java
+++ src/main/java/us/com/rclabs/app/routes/RouteConfig.java
@@ -6,28 +6,46 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
+/**
+ * Mano, essa classe aqui é a RouteConfig Ela manja dos paranauê de configurar as rotas do Camel
+ * pra pegar os XML e mandar umas req SOAP pros brother.
+ */
 @Component
 public class RouteConfig extends RouteBuilder {
 
+    /** Namespace padrão da API? Não mexe não */
     private static final String NAMESPACE = "http://xmlns.oracle.com/oxp/service/v2";
-    private static final String SOAP_ENDPOINT = "spring-ws:https://eksz-test.fa.la1.oraclecloud.com:443/xmlpserver/services/v2/ReportService";
 
+    /** Injeta as config do ambiente, mano. Deus é mais! */
     @Autowired
     private Environment environment;
 
+    /**
+     * Função responsa que configura a rota do Camel, tá ligado?
+     *
+     * @throws Exception se der alguma treta na hora de configurar
+     */
     @Override
     public void configure() throws Exception {
+
+        final String SOAP_ENDPOINT = environment.getProperty("oracle.url");
+
         from("timer:xmlPollingTimer?period=5000")
             .routeId("xmlPollingRoute")
-            .log(LoggingLevel.INFO, "Processing new XML file: ${header.CamelFileName}")
+            .log(LoggingLevel.INFO, "Processando arquivo XML da hora: ${header.CamelFileName}")
             .setHeader("SOAPAction", constant(NAMESPACE + "/getListOfSubjectArea"))
             .setHeader("Content-Type", constant("text/xml;charset=UTF-8"))
             .setBody(constant(createXmlPayload()))
             .to(SOAP_ENDPOINT + "?messageFactory=#messageFactory")
-            .log(LoggingLevel.INFO, "SOAP request completed for ${header.CamelFileName}")
+            .log(LoggingLevel.INFO, "Fechou! Requisição SOAP terminada pro arquivo ${header.CamelFileName}")
             .to("log:us.com.rclabs.app.routes?level=INFO");
     }
 
+    /**
+     * Cria o XML maroto pra mandar na req SOAP, tá ligado?
+     * Detalhe no hardcode :sunglasses:
+     * @return XML montado em String pros parça
+     */
     private String createXmlPayload() {
         return String.format("""
             <getListOfSubjectArea xmlns="%s">
