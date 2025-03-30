6f537a9965286d27aa743536d78ec1d5e7d1d58a
Rafael Cardoso <racasantos@icloud.com>
2025-03-29 23:14:49 -0400

    esta bom agora

--- src/main/java/us/com/rclabs/app/ws/WebServiceConfig.java
+++ src/main/java/us/com/rclabs/app/ws/WebServiceConfig.java
@@ -2,15 +2,11 @@ package us.com.rclabs.app.ws;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
+
 import org.springframework.ws.soap.SoapMessageFactory;
 import org.springframework.ws.soap.SoapVersion;
 import org.springframework.ws.soap.saaj.SaajSoapMessageFactory;
-import org.springframework.ws.wsdl.WsdlDefinition;
-import org.springframework.ws.wsdl.wsdl11.SimpleWsdl11Definition;
 
-import java.io.IOException;
 @Configuration
 public class WebServiceConfig {
 
